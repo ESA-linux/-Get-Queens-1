b19e2e2d82842d7c540034f967d40f3261b6ddcf
Ambjoern Skoeld🛡 <protection@firewicca.com>
2020-03-13 18:13:14 +0100

    import-202003131812.log

---
+++ +Get Queens/Widget/haml_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class HamlComponent < ActionView::Component::Base
+  validates :content, presence: true
+
+  def initialize(message:)
+    @message = message
+  end
+end
