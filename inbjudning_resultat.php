01a1c83e6bff6e4b36b06856614ecad488dfbdac
E.S.A Security Group Shield <protection@firewicca.com>
2020-03-16 03:32:29 +0100

    kalender.php

---
+++ inbjudning_resultat.php
@@ -0,0 +1,157 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
+<html>
+	<head>
+		<link rel=stylesheet href="style.css" type="text/css">
+		<title>MSF - Inbjudningar och resultat</title>
+	</head>
+<body>
+
+	<!-- Sidhuvud -->
+	<div id="content">
+	<div id="box_head">
+		
+		<!--div id="padding"> <a href="omforetaget.php">Om oss</a> | <a href="kontakt.php">Kontakt</a> </div-->
+	</div>
+	<div class="img_box_top"></div>
+
+	<!-- Menyn -->
+	<!-- menyn för förstasidan-->
+<div id="box_top">
+
+<div id="menycontainer">
+<ul>
+<li id="active"><a href="main.php"  >Start</a></li>
+<li><a href="om_foreningen.php"  >Om MSF</a></li>
+<li><a href="kontakt.php" >Kontakta MSF</a></li>
+<li><a href="vart_skytte.php" >Vårt skytte</a></li>
+<li><a href="bli_medlem.php" >Bli medlem</a></li>
+<li><a href="inbjudning_resultat.php" id="current">Inbjudn. & resultat</a></li>
+<li><a href="hitta_hit.php" >Hitta hit</a></li>
+<li><a href="medlem/main_medl.php" >Medlemssidorna</a></li>
+</ul>
+</div>
+
+</div>
+
+	<div id="box">
+		&nbsp;
+		<div class="text_box_framed">
+			<h2>Aktuella inbjudningar</h2>
+			<i>MSF har inga inbjudningar just nu.</i>
+		</div>
+
+		<div class="text_box_framed">
+			<h2>Resultat 2019</h2>
+			<ul>
+				<li><a href="dokument/krm-precision-20190901.pdf" target="_blank">Kretsm&auml;sterskap i precision 2019</a> den 1 Septemper 2019</li>
+				<li><a href="dokument/Resultat-Snubbyfalt-2019-07-27.pdf" target="_blank">Snubbyf&auml;lt</a> den 27 Juli 2019</li>
+				<li><a href="dokument/Resultat-KretsM-CB-Sisjofalten-2019.pdf" target="_blank">Sisjöfälten och Kretsmästerskap C&B 2019</a> den 13 April 2019</li>
+			</ul>
+		</div>
+		
+		<div class="text_box_framed">
+			<h2>Resultat 2018</h2>
+			<ul>
+				<li><a href="dokument/MSF_Millsnabb_2018-04-21.pdf" target="_blank">MSF Militär snabbmatch 2018</a> den 21:a April 2018</li>
+			</ul>
+		</div>
+		
+		
+		<div class="text_box_framed">
+			<h2>Resultat 2017</h2>
+			<ul>
+				<li><a href="dokument/VSM2017_resultat.pdf" target="_blank">VSM Fält 2017</a> den 21:a Maj 2017</li>
+				<li><a href="dokument/msf_milsnabb_20170513.pdf" target="_blank">Militär snabbmatch 2017</a> den 13:e Maj 2017</li>
+				<li><a href="dokument/Resultat_gevar_pistolkulan_20170401.pdf" target="_blank">Gevär & PistolKulan 2017</a> den 1:a April 2017</li>
+				<li>
+					<a href="dokument/Banfalt_3_2017.pdf" target="_blank">MSF Banfält 3 2017</a> den 11:e Mars 2017
+					<ul>
+						<li>Rättad 11/3 2017, 17:11 (rättat Skytt 2 & 3 i R3)</li>
+					</ul>
+				</li>
+				<li>
+					<a href="dokument/CB_Banfält_2_2017.pdf" target="_blank">MSF Banfält 2 2017 C & B</a> den 25:e Feb 2017
+					<ul>
+						<li>Rättad 26/2 2017, 12:03 (rättat namn på skytt)</li>
+						<li>Rättad 12/3 2017 (rättat Stdmdl i B)</li>
+					</ul>
+				</li>
+				<li>
+					<a href="dokument/ARS_Banfält_2_2017.pdf" target="_blank">MSF Banfält 2 2017 A,R & S</a> den 25:e Feb 2017
+					<ul>
+						<li>Rättad 26/2 2017, 12:35 (uppdaterat res i A3 samt mdl för A)</li>
+						<li>Rättad 26/2 2017, 12:03 (rättat namn på skytt)</li>
+					</ul>
+				</li>
+				<li>
+					<a href="dokument/Banfalt_1_2017.pdf" target="_blank">MSF Banfält 1 2017</a> den 4:e Feb 2017
+					<ul>
+						<li>Rättad 5/2 2017, 13:13 (ändrat valör på Mdl i D2, lagt till Mdl i A3 samt lagt till klubbnamn)</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="text_box_framed">
+			<h2>Äldre resultat</h2>
+			<ul>
+				<li><a href="dokument/Resultat_Rundkulan_2016.pdf" target="_blank">Rundkulan 2016</a> den 21:e Maj 2016</li>
+				<li><a href="dokument/slutresultat_sisjofalten_2016.pdf" target="_blank">Resultat Sisjöfälten 2016</a> den 14:e Maj 2016</li>
+				<li><a href="dokument/resultatfil_gevars_o_pistolkulan_2016.pdf" target="_blank">Gevär & pistolkulan 2016.pdf</a> den 3:e April 2016 (pdf)</li>
+				<li><a href="dokument/MSF_banfalt1_2016.pdf" target="_blank">MSF Banfält 1 2016.pdf</a> den 27:e Feb 2016 (pdf)</li>
+
+				<li><a href="dokument/2015-05-02_Rundkulan.pdf" target="_blank">Rundkulan</a> den 2 maj 2015 (pdf)</li>
+				<li><a href="dokument/2015-04-18_Gevars_ o_Pistolkulan.pdf" target="_blank">Gevärs och Pistolkulan</a> den 18 april 2015 (pdf)</li>
+				<li><a href="dokument/2015-04-11_Sisjofalten_v2.pdf" target="_blank">Sisjöfälten</a> den 11 april 2015 (pdf)</li>
+				<li><a href="dokument/2015-02-14_Banfalt_2_2015.pdf" target="_blank">Banfält 2</a> den 14 februari 2015 (pdf)</li>
+				<li><a href="dokument/2015-01-17_Banfalt_1_2015.pdf" target="_blank">Banfält 1</a> den 17 januari 2015 (pdf)</li>
+				<li><a href="dokument/2014-08-31_halgeranet.pdf" target="_blank">Hälgerånet</a> den 31 augusti 2014 (pdf)</li>
+				<li><a href="dokument/2014-08-16_krm_prec.pdf" target="_blank">Kretsmästerskap precision</a> den 16 augusti 2014 (pdf)</li>
+				<li><a href="dokument/2014-05-25_resultat_msf_rundkulan.pdf" target="_blank">Rundkulan</a> den 25 maj 2014 (pdf)</li>
+				<li><a href="dokument/Resultat_SSF2014def-2.pdf" target="_blank">Sisjöfälten</a> den 12 april 2014 (pdf)</li>
+				<li><a href="dokument/Resultat_gevars-_och_pistolkulan_2014_och_remington_cup_1.pdf" target="_blank">Gevärs och Pistolkulan</a> den 5 april 2014 (pdf)</li>
+				<li><a href="dokument/Banfalt_2_2014.pdf" target="_blank">Banfält 2</a> den 22 februari 2014 (pdf)</li>
+				<li><a href="dokument/Banfalt_1_2014_rev2.pdf" target="_blank">Banfält 1</a> den 25 januari 2014 (pdf) *UPPDATERAD*</li>
+				<li><a href="dokument/resultat_halgeranet_2013.pdf" target="_blank">Hälgerånet</a> den 1 september 2013 (pdf) </li>
+				<li><a href="dokument/Resultat_Rundkulan_2013.pdf" target="_blank">Rundkulan</a> den 18 maj 2013 (pdf) </li>
+				<li><a href="dokument/resultat_SSF2013def.pdf" target="_blank">Sisjöfälten</a> den 20 april 2013 (pdf) </li>
+				<li><a href="dokument/Resultat_Gevars-_och_Pistolkulan_2013.pdf" target="_blank">Gevärs och pistolkulan</a> den 16 mars 2013 (pdf) </li>
+				<li><a href="dokument/Resultat_Banfalt1_130126.pdf" target="_blank">Banfält</a> den 26 januari 2013 (pdf) *Rättad*</li>
+				<li><a href="dokument/Resultat_KrM_Mil_snabb_2012.pdf" target="_blank">KrM Militär snabbmatch</a> den 13 oktober 2012 (pdf)</li>
+				<li><a href="dokument/Resultat_Helge12.pdf" target="_blank">Hälgerånet 2012</a> den 1 september 2012 (pdf)</li>
+				<li><a href="dokument/Resultat_Rundkulan12.pdf" target="_blank">Rundkulan </a> söndagen den 13 maj 2012 (pdf)</li>
+				<li><a href="dokument/Resultat_Sisjofalten_12.pdf" target="_blank">Sisjöfälten </a> lördagen den 21 april 2012 (pdf)</li>
+				<li><a href="dokument/Resultat_Gevars-pistolkulan_2012.pdf" target="_blank">Gevärs & pistolkulan </a> Söndagen den 18 Mars 2012 (pdf)</li>
+				<li><a href="dokument/Resultat_Helge11.pdf" target="_blank">Hälgerånet 2011</a> den 10 september 2011 (pdf)</li>
+				<li><a href="dokument/Resultat_Rundkulan11.pdf" target="_blank">Rundkulan 2011</a> den 14 maj 2011 (pdf)</li>
+				<li><a href="dokument/Resultat_gevarskulan-2011.pdf" target="_blank">Gevärskulan 2011</a> den 19 februari 2011 (pdf)</li>
+				<li><a href="dokument/Resultat_Helge10.pdf" target="_blank">Hälgerånet 2010</a> den 4 september 2010 (pdf)</li>
+				<li><a href="dokument/resultat_KrM_prec10.pdf" target="_blank">Kretsmästerskap i Precision 2010</a> den 14 augusti 2010 (pdf)</li>
+				<li><a href="dokument/Resultat_Rundkulan-10.pdf" target="_blank">Rundkulan 2010</a> den 15 maj 2010 (pdf)</li>
+				<li><a href="dokument/Resultat_Sisjofalten_10.pdf" target="_blank">Sisjöfälten 2010</a> den 20 mars 2010 (pdf)</li>
+				<li><a href="dokument/Resultat_Gevarskul2010.pdf" target="_blank">Gevärskulan 10</a> den 14 februari 2010 (pdf)</li>
+				<li><a href="dokument/Helge09.pdf" target="_blank">Hälgerånet 09</a> den 5 september 2009 (pdf)</li>
+				<li><a href="dokument/Resultat_Rundkulan-09.pdf" target="_blank">Rundkulan 09</a> den 16 maj 2009 (pdf)</li>
+				<li><a href="dokument/SF09_resultat.pdf" target="_blank">Sisjöfälten</a> den 14 mars 2009 (pdf)</li>
+				<li><a href="dokument/Resultat_gevarskulan-09.pdf" target="_blank">Gevärskulan</a> den 15 februari 2009 (pdf)</li>
+				<li><a href="dokument/resultat_helge08.pdf" target="_blank">HälgeRånet</a> s&ouml;ndagen den 7 september 2008 (pdf)</li>
+				<li><a href="dokument/Resultat_Rundkulan-08.pdf" target="_blank">Rundkulan</a> l&ouml;rdagen den 17 maj 2008 (pdf)</li>
+				<li><a href="dokument/Resultat_Sisjofalten_08.pdf" target="_blank">Sisj&ouml;f&auml;lten</a> l&ouml;rdagen den 15 mars 2008 (pdf)</li>
+				<li><a href="dokument/Resultat_gevarskulan-08.pdf" target="_blank">Gevärskulan</a> den 16 februari 2008 (pdf)</li>
+				<li><a href="dokument/KrMPrec2007.pdf" target="_blank">Kretsmästeskap i precision</a> den 18 augusti 2007 (pdf)</li>
+				<li><a href="dokument/Resultat_Sisjofalten.pdf" target="_blank">Sisj&ouml;f&auml;lten</a> l&ouml;rdagen den 10 mars 2007 (pdf)</li>
+			</ul>
+		</div>
+		&nbsp;
+		<!-- Sidfot -->
+					</div>
+			<div class="footer">
+				<div class="padding">
+					<a href="mailto:webmaster@molndalsskytte.se?subject=molndalsskytte.com">&copy; Mölndals skytteförening</a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+	</body>
+</html>
